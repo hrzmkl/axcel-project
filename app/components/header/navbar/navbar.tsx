e26c6174a430b6c123d389da8c404876cf2aa48e
zunih <zunih11luis@gmail.com>
2024-11-25 14:25:05 +0300

    Update navbar.tsx

--- app/components/header/navbar/navbar.tsx
+++ app/components/header/navbar/navbar.tsx
@@ -1,5 +1,18 @@
 export default function NavBar(){
     return(
-        <h2>Main navigation</h2>
+        <>
+           <nav className="py-2 bg-light border-bottom">
+                <div className="container d-flex flex-wrap">
+                <ul className="nav  d-flex">
+                    <li className="nav-item"><a href="#" className="nav-link link-dark px-2 active" aria-current="page">Home</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Features</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Pricing</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link link-dark px-2">FAQs</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link link-dark px-2">About</a></li>
+                </ul>
+                
+                </div>
+            </nav> 
+        </>
     );
 }
