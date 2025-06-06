d92f0660e5313b72fa502521ff528deea260c15c
zunih <zunih11luis@gmail.com>
2025-06-06 16:20:42 +0300

    Add keywords

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,20 @@ import Properties from "./components/sections/Properties/PropertieSection";
 export const metadata = {
     title: 'Conseil et assistance en création de société / entreprise à Madagascar',
     description: 'Une société dédiée à fournir des services professionnels aux entreprise',
+    keywords: [
+    'création société',
+    'création entreprise',
+    'assistance entreprise',
+    'assistance société',
+    'conseil en entreprise',
+    'conseil en société',
+    'ouvrir une société',
+    'Services professionnels entreprises',
+    'Conseil assistance création de société',
+    'Conseil assistance création d’entreprise',
+    'conseil assistance administrative',
+    'Coursier administratif',
+  ],
   }
   
 export default function Page() {
